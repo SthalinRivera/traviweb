95b954c130b79859ed834891b1915ee1b91b9dfd
sthalin <sthalin.11@gmail.com>
2023-09-30 19:03:41 -0500

    s

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ import * as FaIcons from 'react-icons/fa';
 import Portal from "./components/Portal";
 import Footer from "./components/Footer";
 import Chatbot from "./components/Chatbot";
-import Modal from "./components/Modal";
 import * as FcIcons from "react-icons/fc";
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
@@ -32,30 +31,7 @@ export default function Home() {
       </Head>
       <Nav/>
       <Portal />
-      <div className="container">
-        <button className={cssBot.floating_button} onClick={showModal ? closeModal : openModal}>
-          {/* Utiliza el estado "isRobotIcon" para mostrar el ícono apropiado */}
-          {isRobotIcon ? <FaIcons.FaRobot /> : <FaIcons.FaTimes />}
-        </button>
-        <div className={`${cssBot.floating_chat_position}`}>
-          <div className={`m-0 p-0   ${showModal ? 'show' : ''}`} tabIndex="-3" role="dialog" style={{ display: showModal ? 'block' : 'none' }}>
-            <div className="modal-dialog " role="document">
-              <div className="modal-content bg-dark ">
-                <div className={`modal-header ${cssBot.chat_bg_header_dark}`} >
-                  <span className="p-2 m-2">
-                    <h5 className="modal-title mx-auto text-light">
-                      <FcIcons.FcReddit /> Chatea con Travi IA
-                    </h5>
-                  </span>
-                </div>
-                <div className="modal-body ">
-                  <Chatbot />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+
       <Footer />
     </div>
   );
