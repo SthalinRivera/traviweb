e19c47eef03e747537931d0c212a92891da670d9
sthalin <sthalin.11@gmail.com>
2023-09-30 21:30:19 -0500

    s

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,16 @@ import Footer from "./components/Footer";
 import Chatbot from "./components/Chatbot";
 import * as FcIcons from "react-icons/fc";
 export default function Home() {
- 
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div id="componente" className={`${styles.gradient_background_dark}`}>
       <Head>
@@ -18,6 +27,51 @@ export default function Home() {
         <link rel="icon" href="/traviweb_logo.png" />
       </Head>
       <Nav/>
+      <button className="btn btn-primary" onClick={openModal}>
+        Abrir modal
+      </button>
+
+      {/* Modal */}
+      {showModal && (
+        <div
+          className="modal fade show"
+          tabIndex="-1"
+          role="dialog"
+          style={{ display: 'block' }}
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Modal title</h5>
+                <button
+                  type="button"
+                  className="close"
+                  onClick={closeModal}
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">Contenido del modal...</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={closeModal}
+                >
+                  Cerrar
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Guardar cambios
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {showModal && (
+        <div className="modal-backdrop fade show"></div>
+      )}
       <Portal />
 
       <Footer />
